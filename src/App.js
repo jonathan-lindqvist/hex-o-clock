216730b7eb664590fe197ec95f9cd3e33a23ca85
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-02-26 13:12:43 +0100

    basic function for hex clock implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  // let [time, setTime]  = useState(new Date())
+  let [hex, setHex] = useState("")
+  
+  const tick = () => {
+    const date = new Date()
+
+    let hex =  "#" + (date.getHours()<10?'0':'') + date.getHours() + (date.getMinutes()<10?'0':'') + date.getMinutes() + (date.getSeconds()<10?'0':'') + date.getSeconds()
+
+    setHex(hex)
+  }
+  
+  useEffect(() => {
+    setInterval(() => tick(), 1000);
+  })
+
   return (
-    <div className="App">
-      hello world
+    <div className="App" style={{backgroundColor: hex}}>
+      {hex}
     </div>
   );
 }
 
-export default App;
+export default App
