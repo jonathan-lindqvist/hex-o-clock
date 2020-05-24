b34541108a3407021f6e68d8d427e4e1bd8b9f25
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-05-24 14:05:12 +0200

    Updates

---
+++ precache-manifest.63ec9a09fc571813ef8ee0b187e38aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551eeda63bca710dd0000e550212062d",
+    "url": "/hex-o-clock/index.html"
+  },
+  {
+    "revision": "305b7d964a3cd82eff5c",
+    "url": "/hex-o-clock/static/css/main.e49b5e48.chunk.css"
+  },
+  {
+    "revision": "fcac83b62c90e237a0d2",
+    "url": "/hex-o-clock/static/js/2.ff505885.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hex-o-clock/static/js/2.ff505885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305b7d964a3cd82eff5c",
+    "url": "/hex-o-clock/static/js/main.2a8760a7.chunk.js"
+  },
+  {
+    "revision": "1354ba260ea3b2c6de06",
+    "url": "/hex-o-clock/static/js/runtime-main.917a32ac.js"
+  }
+]);
